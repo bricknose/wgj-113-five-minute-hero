b22d164a09e427fe32178db75d4ba7b0e20fdf67
bricknose <me@bricknose.com>
2019-09-08 19:05:41 -0500

    Add essence matches.

---
+++ source/JustAddWater/Assets/Scripts/EssenceMatch.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/EssenceMatch", order = 2)]
+public class EssenceMatch : ScriptableObject
+{
+    public Essence PrimaryMatch;
+    public Essence SecondaryMatch;
+    public Essence MatchResult;
+}
